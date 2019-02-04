e6a7ae88a8d8ffc4b8bb2df552b3b080795024a4
Lenny L. Miller <lenny.l.miller@gmail.com>
2019-02-04 13:08:03 -0800

    getting baselined

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ var uniqueRandomArray = require('unique-random-array')
 var starWarsNames = require('./starwars-names.json')
 
 module.exports = {
-    all: starWarsNames,
-    random: uniqueRandomArray(starWarsNames)
+  all: starWarsNames,
+  random: uniqueRandomArray(starWarsNames)
 }
